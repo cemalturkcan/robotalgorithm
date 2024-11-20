3b3083d8d6a4f7c87bd0fa4a10e1519e5f2b70fa
Cemal <cemalturkcann@gmail.com>
2024-11-20 14:46:01 +0300

    join deleted

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,9 @@ public class Main {
         Line line = new Line(List.of(robot1.location, robot2.location));
         Thread robot1Thread = new Thread(new RobotTask(robot1, robot2, Direction.RIGHT, 1));
         Thread robot2Thread = new Thread(new RobotTask(robot2, robot1, Direction.LEFT, 2));
+
         robot1Thread.start();
         robot2Thread.start();
-        robot2Thread.join();
-        robot1Thread.join();
     }
 }
 
